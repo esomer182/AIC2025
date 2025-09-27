ee084c75bb4e38c5c4fe07e542d01dc24e670ce4
Alejandro Vivero <96777983+esomer182@users.noreply.github.com>
2025-09-27 15:33:07 +0200

    Update UnitPlayer.java

--- UnitPlayer.java
+++ UnitPlayer.java
@@ -12,7 +12,20 @@ public class UnitPlayer {
 
     public void run(UnitController uc) {
         Unit unit = new Gatherer(uc);
+        String lstType = "G";
         while (true) {
+            unit.play();
+            if (lstType != unit.type) {
+                if (unit.type == "S") {
+                    unit = new Shoveler(uc);
+                } else if (unit.type == "AT") {
+                    unit = new Attacker(uc);
+                } else if (unit.type == "P") {
+                    unit = new Pickaxer(uc);
+                } else if (unit.type == "AX") {
+                    unit = new Axer(uc);
+                }
+            }
             unit.play();
             uc.yield();
         }
